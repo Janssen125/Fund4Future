7ca7531ce21d0438b99950a0febdffca018ed409
Janssen125 <janssenaddisonchen@gmail.com>
2025-02-13 12:52:18 +0700

    Just Laravel, No React or Typescript :)

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Fund4Future
 AOL Software Engineering
-
-BTW Laravel ini support component dari react, so feel free to use them ;)
-
-https://ui.shadcn.com/docs/components
--- resources/js/app.tsx
+++
@@ -1,9 +0,0 @@
-require('./bootstrap');
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-function App() {
-    return <h1>Hello, Fund4Future!</h1>;
-}
-
-ReactDOM.createRoot(document.getElementById("app")!).render(<App />);
