6f5b13ddc7e8f3007c754d6ffd243f88ee40107d
MelvinSven <melvin.kho@binus.ac.id>
2025-02-25 12:48:25 +0700

    Frontend : Navbar

--- Fund4Future/resources/views/layouts/user.blade.php
+++ Fund4Future/resources/views/layouts/user.blade.php
@@ -9,14 +9,35 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
+<!-- Navbar Section -->
 <header>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid mx-4">
             <a class="navbar-brand" href="#">
-                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24"
-                    class="d-inline-block align-text-top">
-                Bootstrap
+                <img src="{{ asset('img/LogoFund4Future.jpg') }}" alt="Bootstrap" width="50" height="50">
             </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Donation List</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <a class="btn btn-outline-success ms-lg-3" href="{{ route('login') }}">Start Funding</a>
+            </div>
         </div>
     </nav>
 </header>
@@ -25,5 +46,9 @@
     @yield('content')
 </body>
 
+<!-- Footer Section -->
+ <footer>
+    
+ </footer>
 </html>
 {{-- Ini buat templatenya user page (tampilannya user) --}}
