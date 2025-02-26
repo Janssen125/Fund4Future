ff96add177b42fdadc3178c9f70e212e328b36e0
MelvinSven <melvin.kho@binus.ac.id>
2025-02-26 16:58:27 +0700

    Frontend : Dashboard Hero
    janseen

--- Fund4Future/resources/views/layouts/user.blade.php
+++ Fund4Future/resources/views/layouts/user.blade.php
@@ -8,14 +8,18 @@
     <title>@yield('title')</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/root.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 
 <!-- Navbar Section -->
-<header>
+<header class="fixed-top">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid mx-4">
             <a class="navbar-brand" href="#">
-                <img src="{{ asset('img/LogoFund4Future.png') }}" alt="Bootstrap" width="50" height="50">
+                <img src="{{ asset('img/LogoFund4Future.png') }}" alt="Bootstrap" width="45" height="50">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- Fund4Future/resources/views/user/dashboard.blade.php
+++ Fund4Future/resources/views/user/dashboard.blade.php
@@ -3,6 +3,25 @@
     Dashboard
 @endsection
 @section('content')
-    This is Dashboard
+<div class="container-fluid dvh-100 d-flex flex-sm-row w-100 align-items-center container-color">
+    <div class="hero-left w-60 d-flex justify-content-center">
+        <div class="big-hero-left">
+            <div class="p-container">
+                <p class="small-p">#1 fundraiser for technology</p>
+            </div>
+            <div class="h1-container text-wrap">
+                <h1 class="lh-sm fw-semibold header-f">Fund Inovative Future <br> Projects With Us</h1>
+            </div>
+            <div class="button-container">
+                <button type="button" class="btn-ourbtn btn btn-success">Start Funding</button>
+            </div>
+        </div>
+    </div>
+    <div class="hero-right w-40">
+        <div class="big-hero-right">
+            <img src="" alt="">
+        </div>
+    </div>
+</div>
 @endsection
 {{-- Halaman untuk landing page user --}}
