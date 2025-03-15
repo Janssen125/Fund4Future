6a52f135a3180bd081e3c789bda710cc9b17db96
Janssen125 <janssenaddisonchen@gmail.com>
2025-03-15 17:48:38 +0700

    https i hope it work

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -3,9 +3,23 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustHosts as Middleware;
+use Illuminate\Http\Request;
 
 class TrustHosts extends Middleware
 {
+        /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*'; // Trust all proxies
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
     /**
      * Get the host patterns that should be trusted.
      *
