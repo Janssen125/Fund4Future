846b486db4cf1eece1307eee4a742340cf06e567
Janssen125 <janssenaddisonchen@gmail.com>
2025-03-23 13:52:55 +0700

    backend: remember me

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -55,10 +55,6 @@
                     </span>
                 @enderror
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" name="remember" id="remember">
-                <label class="form-check-label" for="remember">Remember Me</label>
-            </div>
             <button type="submit" class="btn btn-primary sign-in-btn w-100">Sign Up</button>
             <hr>
             <div class="container text-center">
