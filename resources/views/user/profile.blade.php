f867efde954200c4ad6c98546bae3d0dfa1bf3d2
Janssen125 <janssenaddisonchen@gmail.com>
2025-04-26 15:47:25 +0700

    Frontend: Profile Page

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -7,55 +7,92 @@
 @endsection
 @section('content')
     <aside>
-        <div class="profile">
-            <img src="{{ asset('img/LogoFund4Future.png') }}" alt="profile picture" width=40 height=40>
-            <h2>{{ Auth::user()->name }}</h2>
-            <p>{{ Auth::user()->email }}</p>
-        </div>
-        <div class="menu">
-            <ul>
-                <li><a href="">Profile</a></li>
-                <li><a href="">Edit Profile</a></li>
-                <li><a href="">Change Password</a></li>
-                <li><a href="">Funding History</a></li>
-                <li><a href="">Funding List</a></li>
-                <li><a href="">Funding Detail</a></li>
-                <li><a href="">Funding Transaction</a></li>
-                <li><a href="">Funding Transaction Detail</a>
-                </li>
-                <li><a href="">Funding Transaction History</a>
-                </li>
-                <li><a href="">Funding Transaction
-                        History Detail</a></li>
-                <li><a href="">F
-                        unding Transaction History List</a></li>
-                <li><a href="">
-                        Funding Transaction History List Detail</a></li>
-                <li><a href="">
-                        Funding Transaction History List Detail Edit</a></li>
-                <li><a href="">
-                        Funding Transaction History List Detail Delete</a></li>
-            </ul>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="profile">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <img src="{{ asset('img/LogoFund4Future.png') }}" alt="profile picture" width=60
+                                        height=60>
+                                </div>
+                                <div class="col col-l">
+                                    <div class="row">
+                                        <div class="col col-l">
+                                            <b>{{ Auth::user()->name }}</b>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col col-l">
+                                            {{ Auth::user()->email }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="menu">
+                        <ul>
+                            <li><a href="">Profile</a></li>
+                            <li><a href="">Change Password</a></li>
+                            <li><a href="">Funding List</a></li>
+                            <li><a href="">Funding Transaction History</a></li>
+                            <li><a href="">Funding History</a></li>
+                            <li><a href="">Settings</a></li>
+                            <li><a href="">Help</a></li>
+                            <li><a href="">Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col">
+                    <span class="customborder"></span>
+                </div>
+            </div>
         </div>
     </aside>
     <section>
         <div class="container">
-            <h1>Profile</h1>
-            <form action="" method="POST">
-                @csrf
-                @method('PUT')
-                <div class="mb-3">
-                    <label for="name" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="name" name="name"
-                        value="{{ Auth::user()->name }}">
+            <div class="row">
+                <div class="col col-l">
+                    <h1>Profile</h1>
                 </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" name="email"
-                        value="{{ Auth::user()->email }}">
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="profiletop">
+                        <div class="row">
+                            <div class="col">
+                                <img src="{{ asset('img/LogoFund4Future.png') }}" alt="profile picture" width=100
+                                    height=100>
+                            </div>
+                        </div>
+                        <div class="row balfund">
+                            <div class="col">Balance: </div>
+                            <div class="col">Total Funded: </div>
+                        </div>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Update Profile</button>
-            </form>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <form action="" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" name="name"
+                                value="{{ Auth::user()->name }}">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" name="email"
+                                value="{{ Auth::user()->email }}">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Update Profile</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
