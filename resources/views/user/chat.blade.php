24006002c0fd859462640870681e9a1d58fba88e
Janssen125 <janssenaddisonchen@gmail.com>
2025-05-14 12:57:27 +0700

    tes doang

--- resources/views/user/chat.blade.php
+++ resources/views/user/chat.blade.php
@@ -7,4 +7,15 @@
 @endsection
 @section('content')
     {{-- Masukkin disini  --}}
-@endsection
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Chat</h1>
+                <div id="chat-container">
+                    <div id="messages"></div>
+                    <input type="text" id="message-input" placeholder="Type a message...">
+                    <button id="send-button">Send</button>
+                </div>
+            </div>
+        </div>
+    @endsection
