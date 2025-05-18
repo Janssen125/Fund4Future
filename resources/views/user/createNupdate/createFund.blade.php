ba144593f97315c76c2ef73486f3944ec5cb575d
Janssen125 <janssenaddisonchen@gmail.com>
2025-05-18 15:30:47 +0700

    backend: integrating frontend and backend

--- resources/views/user/createNupdate/createFund.blade.php
+++ resources/views/user/createNupdate/createFund.blade.php
@@ -5,6 +5,9 @@
 @section('cssName')
     profile
 @endsection
+@section('jsName')
+    createFundUser
+@endsection
 @section('content')
     <aside>
         <div class="container">
@@ -84,7 +87,7 @@
             </div>
             <div class="row">
                 <div class="col">
-                    <form action="{{ route('fund.store') }}" method="POST">
+                    <form action="{{ route('fund.store') }}" method="POST" enctype="multipart/form-data">
                         @csrf
 
                         <!-- Fund Name -->
@@ -93,7 +96,7 @@
                             <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
                                 name="name" value="{{ old('name') }}" required>
                             @error('name')
-                                <div class="text-danger">{{ $message }}</div>
+                                <div class="text-danger show">{{ $message }}</div>
                             @enderror
                         </div>
 
@@ -103,7 +106,7 @@
                             <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
                                 rows="4" required>{{ old('description') }}</textarea>
                             @error('description')
-                                <div class="text-danger">{{ $message }}</div>
+                                <div class="text-danger show">{{ $message }}</div>
                             @enderror
                         </div>
 
@@ -116,12 +119,12 @@
                                 @foreach ($categories as $category)
                                     <option value="{{ $category->id }}"
                                         {{ old('category_id') == $category->id ? 'selected' : '' }}>
-                                        {{ $category->name }}
+                                        {{ $category->catName }}
                                     </option>
                                 @endforeach
                             </select>
                             @error('category_id')
-                                <div class="text-danger">{{ $message }}</div>
+                                <div class="text-danger show">{{ $message }}</div>
                             @enderror
                         </div>
 
@@ -131,9 +134,42 @@
                             <input type="number" class="form-control @error('targetAmount') is-invalid @enderror"
                                 id="targetAmount" name="targetAmount" value="{{ old('targetAmount') }}" required>
                             @error('targetAmount')
-                                <div class="text-danger">{{ $message }}</div>
+                                <div class="text-danger show">{{ $message }}</div>
                             @enderror
                         </div>
+
+                        <!-- Fund Details -->
+                        <div id="fundDetailsContainer">
+                            <h5>Fund Details</h5>
+                            <div class="fund-detail mb-3">
+                                <label for="fund_details[0][types]" class="form-label">Type</label>
+                                <select class="form-select @error('fund_details.0.types') is-invalid @enderror"
+                                    name="fund_details[0][types]" required>
+                                    <option value="" disabled selected>Select Type</option>
+                                    <option value="image" {{ old('fund_details.0.types') == 'image' ? 'selected' : '' }}>
+                                        Image</option>
+                                    <option value="video" {{ old('fund_details.0.types') == 'video' ? 'selected' : '' }}>
+                                        Video</option>
+                                </select>
+                                @error('fund_details.0.types')
+                                    <div class="text-danger show">{{ $message }}</div>
+                                @enderror
+
+                                <label for="fund_details[0][imageOrVideo]" class="form-label mt-2">Upload File</label>
+                                <input type="file"
+                                    class="form-control @error('fund_details.0.imageOrVideo') is-invalid @enderror"
+                                    name="fund_details[0][imageOrVideo]" accept="image/*" onchange="previewImage(event, 0)"
+                                    required>
+                                @error('fund_details.0.imageOrVideo')
+                                    <div class="text-danger show">{{ $message }}</div>
+                                @enderror
+
+                                <!-- Preview Section -->
+                                <div id="previewContainer0" class="mt-3"></div>
+                            </div>
+                        </div>
+
+                        <button type="button" id="addFundDetail" class="btn btn-secondary mb-3">Add More Details</button>
 
                         <!-- Submit Button -->
                         <button type="submit" class="btn btn-primary">Create Fund</button>
