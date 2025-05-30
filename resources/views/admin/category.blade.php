986571dff2e106d9f6d16129f2ceffd654ab36b8
Janssen125 <janssenaddisonchen@gmail.com>
2025-05-30 22:52:08 +0700

    Frontend: kasih basicnya untuk kerjain category list

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -2,7 +2,21 @@
 @section('title')
     Category List
 @endsection
+@section('cssName')
+    admindashboard
+@endsection
 @section('content')
-    This is Category List
+    <div>
+        <h1 class="p-5">
+            Category
+        </h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                {{-- Good Luck --}}
+            </div>
+        </div>
+    </div>
 @endsection
 {{-- Halaman ini untuk mengatur kategori penggalangan dana, kayaknya admin only ini --}}
